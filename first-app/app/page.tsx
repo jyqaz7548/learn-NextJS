0a0a692edb8b971b590eee0c56c7c38dcfca3475
jyqaz7548 <24robot10507@sonline20.sen.go.kr>
2024-08-21 20:12:47 +0900

    good

--- first-app/app/page.tsx
+++ first-app/app/page.tsx
@@ -2,5 +2,10 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <div>Hello World!</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>오늘의기분</h1>
+      <p>sad</p>
+    </div>
+  );
 }
