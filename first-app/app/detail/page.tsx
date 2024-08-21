e8e4b81e035c3d791f19d1f2275580e0afd60485
jyqaz7548 <24robot10507@sonline20.sen.go.kr>
2024-08-21 20:36:34 +0900

    cummit

--- first-app/app/detail/page.tsx
+++ first-app/app/detail/page.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import styles from "./detail.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>오늘의기분222222222222222222</h1>
-      <p>good</p>
+      <Link href="/">good</Link>
     </div>
   );
 }
--- first-app/app/page.tsx
+++ first-app/app/page.tsx
@@ -3,10 +3,26 @@ import styles from "./page.module.css";
 import Link from "next/link";
 
 export default function Home() {
+  const value = "typescript 최고saddsa";
+  const numlist: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
   return (
     <div className={styles.container}>
+      {numlist.map((value, i) => {
+        console.log(value, i);
+        return (
+          <div>
+            <h1>2단</h1>
+            <h2>2*1=2</h2>)
+            <br />
+          </div>
+        );
+      })}
+
       <h1 className={styles.title}>오늘의기분</h1>
       <Link href="/detail">sad</Link>
+      <p>{value}</p>
+      <br />
     </div>
   );
 }
