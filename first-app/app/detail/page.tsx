7cce0550fd9c5e623269ec58252d0b0ff65d668f
jyqaz7548 <24robot10507@sonline20.sen.go.kr>
2024-08-21 20:16:24 +0900

    new page

---
+++ first-app/app/detail/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import styles from "./detail.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>오늘의기분222222222222222222</h1>
+      <p></p>
+    </div>
+  );
+}
