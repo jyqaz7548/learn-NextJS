eb2e35e2937336fe75827a5af88a15c350344caf
jyqaz7548 <24robot10507@sonline20.sen.go.kr>
2024-08-26 21:12:20 +0900

    코밋

--- first-app/app/my_counter/page.tsx
+++ first-app/app/my_counter/page.tsx
@@ -1,23 +1,27 @@
 "use client";
-import Navbar from "@/components/nevbar";
+import Nevbar from "@/components/nevbar";
 import styles from "./detail.module.css";
-import { use, useState } from "react";
+import { useState } from "react";
 
 export default function counter() {
-  const [value, setValue] = useState(0);
+  const [value, setValue] = useState(100);
 
   const plus = () => {
     setValue(value + 1);
-  };
-
-  const not = () => {
-    setValue(value);
   };
 
   const minus = () => {
     if (value !== 0) {
       setValue(value - 1);
     }
+  };
+
+  const multiply = () => {
+    setValue(value * 2);
+  };
+
+  const division = () => {
+    setValue(value / 2);
   };
 
   const reset = () => {
@@ -29,13 +33,16 @@ export default function counter() {
       <div className={styles.container}>
         <h1>{value}</h1>
         <div className={styles.button}>
-          <button onMouseDown={plus} onMouseUp={not}>
-            +
-          </button>
+          <button onMouseDown={plus}>+</button>
           <button onMouseDown={minus}>-</button>
+          <button onClick={multiply}>x</button>
+          <input type="text" placeholder="입력해라" />
+          <button onClick={division}>÷</button>
           <button onClick={reset}>reset</button>
         </div>
       </div>
     </>
   );
 }
+
+//ㅇㅇ
