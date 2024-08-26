e678ce7672ac4190b99f74bac59896afbe0a127f
jyqaz7548 <24robot10507@sonline20.sen.go.kr>
2024-08-26 21:43:39 +0900

    끝냈다아아아아아아아아아아아아아아

--- first-app/app/my_counter/page.tsx
+++ first-app/app/my_counter/page.tsx
@@ -4,10 +4,10 @@ import styles from "./detail.module.css";
 import { useState } from "react";
 
 export default function counter() {
-  const [value, setValue] = useState(100);
+  const [value, setValue] = useState(0);
 
   const plus = () => {
-    setValue(value + 1);
+    setValue(value + 100);
   };
 
   const minus = () => {
@@ -17,11 +17,23 @@ export default function counter() {
   };
 
   const multiply = () => {
-    setValue(value * 2);
+    const input = prompt("곱할 수 입력:");
+    const number = Number(input);
+    if (!isNaN(number)) {
+      setValue(value * number);
+    } else {
+      alert("유효한 숫자를 입력해주세요.");
+    }
   };
 
   const division = () => {
-    setValue(value / 2);
+    const input2 = prompt("나눌 수 입력:");
+    const number2 = Number(input2);
+    if (!isNaN(number2) && number2 !== 0) {
+      setValue(value / number2);
+    } else {
+      alert("유효한 숫자를 입력해주세요.");
+    }
   };
 
   const reset = () => {
@@ -36,7 +48,6 @@ export default function counter() {
           <button onMouseDown={plus}>+</button>
           <button onMouseDown={minus}>-</button>
           <button onClick={multiply}>x</button>
-          <input type="text" placeholder="입력해라" />
           <button onClick={division}>÷</button>
           <button onClick={reset}>reset</button>
         </div>
