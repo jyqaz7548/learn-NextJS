526b92a7c81ffc7d3e79138bf5438a452fbd51f6
jyqaz7548 <24robot10507@sonline20.sen.go.kr>
2024-08-27 16:52:36 +0900

    452

--- first-app/app/detail/page.tsx
+++ first-app/app/detail/page.tsx
@@ -2,6 +2,11 @@ import Image from "next/image";
 import styles from "./detail.module.css";
 import Link from "next/link";
 import Navbar from "@/components/nevbar";
+import ChildComp from "@/components/childComp";
+
+const dates = {name:"기아",rank:1,locate:"광주"},
+              {name:"기아",rank:1,locate:"광주"},
+{name:"기아",rank:1,locate:"광주"},
 
 export default function Home() {
   return (
@@ -9,6 +14,7 @@ export default function Home() {
       <div className={styles.container}>
         <h1 className={styles.title}>오늘의기분222222222222222222</h1>
         <Link href="/">good</Link>
+        <ChildComp />
       </div>
     </>
   );
